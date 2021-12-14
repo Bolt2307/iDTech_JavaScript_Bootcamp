8c4d447debec0c608cebd1d41ea5ae30b124bec7
Bolt2307 <maxroedy@gmail.com>
2021-12-13 16:36:17 -0800

    I did a thing ;)

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,33 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+var greetings = "Welcome to my kingdom!";
+var name = "Flare";
+var title = "King of dragons";
+var worldName = "Warrior World"
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+var age = 2000;
+var year = 15000;
+var century = 150;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponsUsed = ['Sword', 'Wand', 'Ninja Stars', 'Dragon Fruit'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+var hasFavWeapon = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + " My name is " + name + ".");
+console.log("I'm the " + title);
+console.log("As the leader of " + worldName + ", I use different weapons.");
+console.log("Time works differently in my world.");
+console.log("For exampl, I'm " + age + " years old.");
+console.log("In my world, you are in year " + year + ", the " + century + "th century" + "\n");
+console.log("Does anyone have any questions?");
+console.log("Me: Do you have a favorite weapon?");
+console.log("Dragon: " + hasFavWeapon + ", my favorite weapon is " + weaponsUsed[3] + "." + "\n");
+console.log("You are free to explore my world and return to Earth after yor tour is over!");
